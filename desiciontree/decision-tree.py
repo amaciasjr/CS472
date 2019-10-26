f467f8a48f93d2f842f7c3c59c997e728c45f600
Art Macias <art.maciasjr@gmail.com>
2019-10-26 14:28:54 -0600

    Rename file and find best feature for 'Root Node' of DT

--- desiciontree/decision-tree.py
+++ desiciontree/decision-tree.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import log2
 from sklearn.base import BaseEstimator, ClassifierMixin
 from arff import Arff
 from tree import Tree
@@ -46,21 +47,29 @@ class DTClassifier(BaseEstimator, ClassifierMixin):
 
         class_col = np.shape(data_set)[1] - 1
         for feature in range(np.shape(data_set)[1] - np.shape(y)[1]):
+
             feature_info = {}
-            for observation in range(np.shape(data_set)[0]):
+            total_observations = np.shape(data_set)[0]
+            for observation in range(total_observations):
                 observation_val = data_set[observation][feature]
                 class_val = data_set[observation][class_col]
                 if observation_val in feature_info:
+                    feature_info[observation_val]['total'] = feature_info[observation_val]['total'] + 1
                     if class_val in feature_info[observation_val]:
-                        feature_info[observation_val][class_val] = feature_info[observation_val][class_val] + 1
+                        feature_info[observation_val][class_val] = feature_info[observation_val][class_val] + 1.0
                     else:
-                        feature_info[observation_val][class_val] = 1
+                        feature_info[observation_val][class_val] = 1.0
                 else:
-                    feature_info[observation_val] = {class_val: 1}
+                    feature_info[observation_val] = {class_val: 1.0, 'total' : 1}
 
-                # print(f"Feature: {feature}, Observation_value: {data_set[observation][feature]}, Class_value: {data_set[observation][class_col]}")
-            print(f"Feature {feature} Info: {feature_info}")
+            entropy_sum = 0
+            for category in feature_info:
+                entropy_sum = entropy_sum + self._calc_entropy(total_observations, feature_info[category])
 
+            entropies.append(entropy_sum)
+        min_entropy = min(entropies)
+        best_feature = entropies.index(min_entropy)
+        print(best_feature)
         return self
 
     def predict(self, X):
@@ -85,11 +94,25 @@ class DTClassifier(BaseEstimator, ClassifierMixin):
         """
         return 0
 
-    def _calc_entropy(self, p):
-        if p != 0:
-            return -p * np.log2(p)
-        else:
-            return 0
+    # Helper Functions
+    def _calc_entropy(self, all_obs, category_info):
+        category_entropy_sum = 0
+        category_total_obs = category_info['total']
+        del category_info['total']
+
+        for type in category_info:
+            type_obs = category_info[type]
+            probability = type_obs / category_total_obs
+            category_entropy_sum = category_entropy_sum + (category_total_obs / all_obs) * (-(probability)*(log2(probability)))
+
+        return category_entropy_sum
+
+
+    # def _calc_entropy(self, p):
+    #     if p != 0:
+    #         return -p * np.log2(p)
+    #     else:
+    #         return 0
 
 
 if __name__ == '__main__':
